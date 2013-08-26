793f233f4932f5adde27b01b2a96aaf282a0d990
Sergei Lissovski <sergei.lissovski@gmail.com>
2013-08-26 02:17:04 -0700

    README.markdown updated and required annotations now will be loaded automatically when the bundle is booted

--- SliExtJsIntegrationBundle.php
+++ SliExtJsIntegrationBundle.php
@@ -2,6 +2,7 @@
 
 namespace Sli\ExtJsIntegrationBundle;
 
+use Doctrine\Common\Annotations\AnnotationRegistry;
 use Sli\ExpanderBundle\DependencyInjection\CompositeContributorsProviderCompilerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -9,6 +10,19 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class SliExtJsIntegrationBundle extends Bundle
 {
+    public function boot()
+    {
+        $reflClass = new \ReflectionClass(__CLASS__);
+        $path = dirname($reflClass->getFileName());
+
+        AnnotationRegistry::registerFile(
+            $path . '/DataMapping/Annotations.php'
+        );
+        AnnotationRegistry::registerFile(
+            $path . '/Service/SortingFieldAnnotations.php'
+        );
+    }
+
     public function build(ContainerBuilder $container)
     {
         // FIXME ain't good, it must be done rather in RendererProvidersConfigurator
