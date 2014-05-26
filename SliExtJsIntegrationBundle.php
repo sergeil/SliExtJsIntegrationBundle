48bac86cb15c0fe8cde1197492b98a31ba6a8c88
Sergei Lissovski <sergei.lissovski@gmail.com>
2014-05-26 15:32:12 +0300

    [MPFE-609] bundle updated to leverage latest extension-points architecture

--- SliExtJsIntegrationBundle.php
+++ SliExtJsIntegrationBundle.php
@@ -4,6 +4,7 @@ namespace Sli\ExtJsIntegrationBundle;
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Sli\ExpanderBundle\DependencyInjection\CompositeContributorsProviderCompilerPass;
+use Sli\ExpanderBundle\Ext\ExtensionPoint;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\HttpKernel\Kernel;
@@ -35,8 +36,8 @@ class SliExtJsIntegrationBundle extends Bundle
             )
         );
 
-        $container->addCompilerPass(
-            new CompositeContributorsProviderCompilerPass('sli.extjsintegration.complex_field_value_converters_provider')
-        );
+        $valueConverterProviders = new ExtensionPoint('sli.extjsintegration.complex_field_value_converters_provider');
+        $valueConverterProviders->setDescription('Allows to contribute custom value converters');
+        $container->addCompilerPass($valueConverterProviders->createCompilerPass());
     }
 }
